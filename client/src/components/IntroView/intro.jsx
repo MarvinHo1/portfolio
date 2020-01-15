7a4a53f6f0ac44dfb71aefcb2bcd4da7cf8333f7
Marvin Ho <myuanHo1@gmail.com>
2020-01-15 10:42:26 -0800

    Test video background and worked on some CSS

--- client/src/components/IntroView/intro.jsx
+++ client/src/components/IntroView/intro.jsx
@@ -12,10 +12,18 @@ class IntroPage extends React.Component {
 
   render() {
     return (
-      <div className={style.introContainer}>
-        <div className={style.introImage} />
-        <div className={style.introButtonContainer}>
-          <button type="button" className={style.clickMeButton}>Click Me!</button>
+      <div className={style.introComponenet}>
+        <div className={style.introContainer}>
+          <video className={style.videoBG} poster="https://imageportfolio.s3.amazonaws.com/night_hill.jpg" autoPlay muted loop>
+            <source src="https://imageportfolio.s3.amazonaws.com/Night_BG.mp4" type="video/mp4" />
+          </video>
+          <div className={style.introButtonContainer}>
+            <p>
+              Welcome to my profile! my name is Marvin Ho.
+              I am a fullstack engineer.
+            </p>
+            <button type="button" className={style.clickMeButton}>Click Me!</button>
+          </div>
         </div>
       </div>
     );
--- client/src/components/projects/card.jsx
+++ client/src/components/projects/card.jsx
@@ -42,6 +42,26 @@ const Card = (props) => {
             </div>
           </div>
         </div>
+        <div className={style.card}>
+          <div className={style.cardVisual1}>
+            <div className={style.cardDescription}>
+              <h3>Stuff</h3>
+              <img className={style.cardImage} src="http://imageportfolio.s3.amazonaws.com/night_hill.jpg" alt="Card for Testing" />
+            </div>
+          </div>
+          <div className={style.cardVisual2}>
+            <div className={style.cardDescription}>
+              <p>React.js</p>
+              <hr />
+              <p>Node.js</p>
+              <hr />
+              <p>MongoDB</p>
+              <hr />
+              <p>Webpack</p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
