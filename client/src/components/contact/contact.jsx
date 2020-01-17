9cb5ce9844d2385f423c2fe9bde2544a316d4552
Marvin Ho <myuanHo1@gmail.com>
2020-01-17 13:17:17 -0800

    Progress contact module

--- client/src/components/contact/contact.jsx
+++ client/src/components/contact/contact.jsx
@@ -1,38 +1,69 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
 import style from '../../../../stylesheet.css';
+// import key from './email.jsx';
 
 
-export default function ContactUs() {
-
-  function sendEmail(e) {
-    e.preventDefault();
-
-    const templateParams = {
-      from_name: 'James',
-      message_html: 'Check this out!'
+class ContactUs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
     };
-    
+    this.sendEmail = this.sendEmail.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  sendEmail(e) {
+    e.preventDefault();
+    const { name, email, message } = this.state;
+    let templateParams = {
+      to_name: 'Marvin',
+      contact_email: email,
+      from_name: name,
+      message_html: message,
+    };
     emailjs.send('marvin_gmail', 'template_YUkmXCTa', templateParams)
       .then((response) => {
-         console.log('SUCCESS!', response.status, response.text);
+        console.log('SUCCESS!', response.status, response.text);
       }, (err) => {
-         console.log('FAILED...', err);
+        console.log('FAILED...', err);
       });
   }
 
-  return (
-    <form className="contact-form" onSubmit={sendEmail}>
-      <input type="hidden" name="contact_number" />
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-  );
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  render() {
+    const { name, email, message } = this.state;
+    // {console.log(key)}
+    return (
+      <div>
+        <div className={style.aboutTitleContainer}>
+          <h1 className={style.aboutTitle}>
+            <hr />
+              Contact
+            <hr />
+          </h1>
+        </div>
+        <form className="contact-form" value={name} onSubmit={this.sendEmail}>
+          <label>Name</label>
+          <input name="name" value={name} onChange={this.handleChange} />
+          <label>Email</label>
+          <input type="email" name="email" value={email} onChange={this.handleChange} />
+          <label>Message</label>
+          <textarea name="message" value={message} onChange={this.handleChange} />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    );
+  }
 }
 
-
+export default ContactUs;
